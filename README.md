a06f906b3e49e64045517b918a2eb9d0af5bc396
Shish <shish@shishnet.org>
2023-04-07 16:22:19 +0100

    more syncing, and working demo

--- README.md
+++ README.md
@@ -5,8 +5,10 @@ A hook which allows multiple clients to keep their clocks in-sync
 with reasonable precision, eg
 
 ```ts
+import { useServerTime } from "@shish2k/react-use-servertime";
+
 function MyClock() {
-	const { now } = useServerTime("https://karakara.uk/time.json");
+	const { now } = useServerTime({url: "https://karakara.uk/time.json"});
 
 	return <div>The time is {now}</div>;
 }
@@ -15,6 +17,25 @@ function MyClock() {
 The given URL should return the current time in floating-point seconds
 
 This hook will update the `now` value once per second, on the second
+
+If you have multiple places which want to use the time, you can use a
+context provider so that multiple components share a single clock:
+
+```ts
+import { ServerTimeContext, ServerTimeProvider } from "@shish2k/react-use-servertime";
+
+function MyClock() {
+	const { now } = useContext(ServerTimeContext);
+
+	return <div>The time is {now}</div>;
+}
+
+function App() {
+	return <ServerTimeProvider url="https://karakara.uk/time.json">
+		<MyClock />
+	</ServerTimeProvider>
+}
+```
 
 See [demo.html](demo.html) for a self-contained, well-commented example.
 
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,94 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
-export function useServerTime(url: string, interval?: number) {
+export type ServerTimeProps = {
+  url: string;
+  samples?: number;
+  interval?: number;
+  sync?: number;
+};
+
+export type ServerTimeContextType = {
+  now: number;
+  offset: number;
+  tweak: number;
+  setTweak: (x: number) => void;
+};
+
+export function useServerTime(user_props: ServerTimeProps): ServerTimeContextType {
+  const props = {
+    samples: 5,
+    interval: 1,
+    sync: 60,
+    ...user_props,
+  };
+  const [offsets, setOffsets] = useState<number[]>([]);
   const [offset, setOffset] = useState<number>(0);
   const [tweak, setTweak] = useState<number>(0);
   const [now, setNow] = useState<number>(Date.now() / 1000);
 
-  useEffect(() => {
+  // Sync immediately on startup, and then on a schedule if props.sync is set
+  function sync(url: string) {
+    const sent = Date.now() / 1000;
     fetch(url, { credentials: "omit" })
       .then((r) => r.json())
-      .then((t) => setOffset(Date.now() / 1000 - t));
-  }, [url]);
-
+      .then((response) => {
+        const server_time = typeof response == "number" ? response : response.time_s;
+        const recvd = Date.now() / 1000;
+        const ping = (recvd - sent) / 2;
+        const offset = server_time - ping - sent;
+        // TODO: if ping is too large, get a new sample
+        // console.log("New offset", offset, offsets, props.samples);
+        setOffsets((offsets) => {
+          let new_offsets = [...offsets, offset];
+          if (new_offsets.length > props.samples) {
+            new_offsets.shift();
+          }
+          return new_offsets;
+        });
+      });
+  }
   useEffect(() => {
-    const x = setInterval(() => {
-      setNow(Date.now() / 1000 - offset + tweak);
-    }, interval ?? 1000);
+    setOffset(offsets.length ? offsets.reduce((a, b) => a + b, 0) / offsets.length : 0);
+  }, [offsets]);
+  useEffect(() => {
+    sync(props.url);
+    let sync_id: ReturnType<typeof setInterval> | null = null;
+    if (props.sync > 0) {
+      sync_id = setInterval(() => sync(props.url), props.sync * 1000);
+    }
     return () => {
-      clearInterval(x);
+      sync_id && clearInterval(sync_id);
+    }
+  }, [props.url, props.sync]);
+
+  // Update `now` on a regular interval
+  useEffect(() => {
+    var interval_id: ReturnType<typeof setTimeout> | null = null;
+    function waitForNextInterval() {
+      const now = Date.now() / 1000 - offset + tweak;
+      setNow(now);
+      interval_id = setTimeout(
+        waitForNextInterval,
+        (props.interval - (now % props.interval)) * 1000
+      );
+    }
+    waitForNextInterval();
+    return () => {
+      interval_id && clearInterval(interval_id);
     };
-  }, [interval]);
+  }, [props.interval]);
 
   return { now, offset, tweak, setTweak };
+}
+
+export const ServerTimeContext = React.createContext<ServerTimeContextType>({
+  now: 0,
+  offset: 0,
+  tweak: 0,
+  setTweak: () => { },
+});
+
+export function ServerTimeProvider(props: ServerTimeProps & { children: React.ReactNode }) {
+  const value = useServerTime(props);
+  return React.createElement(ServerTimeContext.Provider, { value, children: props.children });
 }
